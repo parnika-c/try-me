372151a7d824d22fda67feef70aadd0121f58547
Deepika Sharma <deepika@Deepikas-MacBook-Pro.local>
2025-10-25 19:51:29 -0700

    added nav bar features

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import { CreateChallengeModal } from './components/CreateChallengeModal'
-import './App.css'
+import { useState } from 'react';
+import { NavBar } from './components/NavBar';
+import './App.css';
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
-
   return (
     <div className="App">
+      <NavBar />
       <div className="main-content">
-        <h1>Try Me</h1>
-
-        <button 
-          className="create-challenge-btn"
-          onClick={() => setShowModal(true)}
-        >
-          Create Challenge
-        </button>
       </div>
-
-      {showModal && (
-        <CreateChallengeModal 
-          onClose={() => setShowModal(false)}
-          onCreateChallenge={(challengeData) => {
-            console.log('Creating challenge:', challengeData)
-            setShowModal(false)
-          }}
-        />
-      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
