6bb05d4ea0c009afc096d8481c47e0177c601abe
Parnika <parnika.chaturvedi@gmail.com>
2025-10-31 12:55:20 -0700

    add back create challenge modal so merging doesn't overwrite main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react';
 import { NavBar } from './components/NavBar';
+import { CreateChallengeModal } from './components/CreateChallengeModal'
 import './App.css';
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <div className="App">
       <NavBar />
       <div className="main-content">
+        <button 
+          className="create-challenge-btn"
+          onClick={() => setShowModal(true)}
+        >
+          Create Challenge
+        </button>
       </div>
+      
+      {showModal && (
+        <CreateChallengeModal 
+          onClose={() => setShowModal(false)}
+          onCreateChallenge={(challengeData) => {
+            console.log('Creating challenge:', challengeData)
+            setShowModal(false)
+          }}
+        />
+      )}
     </div>
   );
 }
