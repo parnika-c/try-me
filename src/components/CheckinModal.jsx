f318304a977403e3882702d10726163a36c6b686
Diya <desaivdiya@gmail.com>
2025-12-02 21:33:13 -0800

    Added recalculatePoints as a one time file because it fixes existing totalPoints value for all users based on their check-ins. Also updated checkins.js so that it sends pointsEarned per each check in to totalPoints in the user schema. Lastly, I had the navBar rerender every time the check-in modal had a user click check in, so that navbar shows real-time data

--- src/components/CheckinModal.jsx
+++ src/components/CheckinModal.jsx
@@ -48,6 +48,9 @@ export function CheckinModal({challenge, currentDay = 0, checkIns = [], onComple
 
       await onComplete(data);
       closeModal();
+
+      // Dispatch event to update navbar
+      window.dispatchEvent(new Event('userDataChanged'));
     } catch (err) {
       console.error("Error checking into challenge:",err.message);
       alert("Failed to check into challenge. Try again.");
