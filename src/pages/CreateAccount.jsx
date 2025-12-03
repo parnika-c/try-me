2b6788cdaafc1b96584426baf0dae5a2ca2001eb
Deepika Sharma <deepika@Deepikas-MacBook-Pro.local>
2025-12-03 12:51:50 -0800

    fixed position of title in create account page

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -109,10 +109,10 @@ const CreateAccount = ({ onAccountCreated, onShowLogin }) => {
         <div className="create-account-hero">
           <div className="create-account-hero-content">
             <h1 className="create-account-hero-title">
-              Welcome to TryMe!, a fun way to compete with your friends!
+              Welcome to TryMe!
             </h1>
             <p className="create-account-hero-text">
-              Create an account to creat challenges and compete with your friends!
+              Create an account to create challenges and compete with your friends!
             </p>
           </div>
         </div>
